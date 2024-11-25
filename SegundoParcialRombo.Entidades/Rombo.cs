90292352530268e1d78e8623e80db877a2ef6f8f
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2024-11-25 20:28:39 -0300

    Agregue clase Rombo

---
+++ SegundoParcialRombo.Entidades/Rombo.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SegundoParcialRombo.Entidades
+{
+    public class Rombo
+    {
+        public int DiagonalMayor { get; set; }
+        public int DiagonalMenor { get; set; }
+        public double Lado { get; set; }
+        public Rombo(int dM,int dm)
+        {
+            DiagonalMayor = dM;
+            DiagonalMenor = dm;
+            Lado = CalcularLado();
+        }
+
+        private double CalcularLado()
+        {
+            return Math.Sqrt(Math.Pow(DiagonalMayor/2,2)+Math.Pow(DiagonalMenor/2,2));
+        }
+        public double GetPerimetro() => 4 * Lado;
+        public double GetArea() => DiagonalMayor * DiagonalMenor / 2;
+    }
+}
